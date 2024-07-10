ae84d7041b142c9656aca4bb9f605df9532a0c9c
Evelyn <heejinevelyn@gmail.com>
2024-07-10 18:34:48 -0300

    Adicionar arquivos de projeto.

---
+++ Calculadora.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoCalculadora
+{
+    public static class Calculadora
+    {
+       public static double Somar(double x, double y)
+        { 
+            return x + y; 
+        }
+        public static double Subtrair(double x, double y)
+        {
+            return x - y;
+        }
+        public static double Multiplicar(double x, double y)
+        {
+            return x * y;
+        }
+        public static double Dividir(double x, double y)
+        {
+            return x / y;
+        }
+
+    }
+}
