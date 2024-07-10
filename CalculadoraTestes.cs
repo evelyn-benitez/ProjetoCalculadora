c7fa689e1ee576653ddc2bb3195452a5ed4ae166
Evelyn Benitez <97813266+evelyn-benitez@users.noreply.github.com>
2024-07-10 19:27:59 -0300

    Add files via upload

---
+++ CalculadoraTestes.cs
@@ -0,0 +1,55 @@
+﻿using Xunit;
+using ProjetoCalculadora;
+
+namespace Teste_xUnit
+{
+
+    /* Testes */
+    public class CalculadoraTestes
+    {
+        [Fact]
+        public void TesteSomar()
+        {
+            double resultado = Calculadora.Somar(2, 3);
+
+            Assert.Equal(5, resultado);
+        }
+
+
+        [Fact]
+        public void TesteSubtrair()
+        {
+            double resultado = Calculadora.Subtrair(5, 3);
+
+            Assert.Equal(2, resultado);
+        }
+
+
+        [Fact]
+        public void TesteMultiplicar()
+        {
+            double resultado = Calculadora.Multiplicar(4, 3);
+
+            Assert.Equal(12, resultado);
+        }
+
+
+
+        [Fact]
+        public void TesteDividir()
+        {
+            double resultado = Calculadora.Dividir(27, 3);
+
+            Assert.Equal(9, resultado);
+        }
+
+
+        [Fact]
+        public void TesteDividirPorZero()
+        {
+            double resultado = Calculadora.Dividir(10, 0);
+
+            Assert.Equal(double.PositiveInfinity, resultado);
+        }
+    }
+}
